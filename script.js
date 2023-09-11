83e24b8935c17802ab35c82ad420e98b79ad8120
Daniel Albano III <captdane3@gmail.com>
2023-09-11 19:52:32 +0800

    practice: added almost last consts

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ const list = document.querySelector('#id')
 const form = document.querySelector('#new-todo-form')
 const todoInput = document.querySelector('#todo-input')
 const template = document.querySelector('#list-item-template')
+
+const LOCAL_STORAGE_PREFIX = 'ADVANCED_TODO_LIST-'
+const TODO_STORAGE_KEY = `${LOCAL_STORAGE_PREFIX}todos`
