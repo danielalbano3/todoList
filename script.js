19be6fb0c8a9f57904498f1d375ae3cc8e06b13d
Daniel Albano III <captdane3@gmail.com>
2023-09-11 19:49:16 +0800

    practice: more consts

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 const list = document.querySelector('#id')
 const form = document.querySelector('#new-todo-form')
+const todoInput = document.querySelector('#todo-input')
+const template = document.querySelector('#list-item-template')
