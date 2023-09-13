3388019a82728aaa5abd42a0b994673a8766e2a5
Daniel Albano III <captdane3@gmail.com>
2023-09-13 23:37:50 +0800

    more functions

--- script.js
+++ script.js
@@ -4,15 +4,27 @@ const todoInput = document.querySelector('#todo-input')
 const template = document.querySelector('#list-item-template')
 
 const LOCAL_STORAGE_PREFIX = 'ADVANCED_TODO_LIST-'
-const TODO_STORAGE_KEY = `${LOCAL_STORAGE_PREFIX}todos`
+const TODOS_STORAGE_KEY = `${LOCAL_STORAGE_PREFIX}todos`
 
 let todos = loadTodos()
 todos.forEach(renderTodo)
 
 function loadTodos(){
-
+  const todosString = localStorage.getItem(TODOS_STORAGE_KEY)
+  return JSON.parse(todosString) || []
 }
 
 function renderTodo(todo){
+  const templateClone = template.content.cloneNode(true)
   
+  const listItem = template.querySelector('.list-item')
+  listItem.dataset.todoId = todo.id
+
+  const textElement = templateClone.querySelector('[data-list-item-text]')
+  textElement.innerText = todo.name
+
+  const checkbox = templateClone.querySelector('[data-list-item-checkbox]')
+  checkbox.checked = todo.complete
+
+  list.appendChild(templateClone)
 }
